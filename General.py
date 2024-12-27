55dc649c3b5608a6cca9885b83cced8ddb9e7cba
2012Miron <gsvitozar@gmail.com>
2024-12-27 16:30:26 +0200

    Update General.py

--- General.py
+++ General.py
@@ -39,7 +39,7 @@ if os.path.exists(folder_path):
     print("Data load sucsesfull.")
 else:
     print("Error. Not found necessary files. Please, check script checker.py, and ackter.py in folder checker.")
-    print("If you have not this files, download it from GitHub. Here link: https://github.com/2012Miron/List-Manager")
+    print("If you haven't this files, download it from GitHub. Here link: https://github.com/2012Miron/List-Manager")
     input("Press Enter to exit...")
     exit()
 
