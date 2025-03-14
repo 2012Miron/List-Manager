31398a883c409442a97c2a7a9817f68cecc919d5
Miron <gsvitozar@gmail.com>
2025-03-14 17:44:19 +0200

    Update General.py
    Updated file to LM 1.3. version.

--- General.py
+++ General.py
@@ -1,24 +1,22 @@
 import os
 import pickle
-import plugins
 import plugins.plugin_main
 
-folder_path = 'data/list1.lmdb'
-ifinfirst = 'data/firstStart.pkl'
-infirst = ""
+folder_path = 'data'
+first_start_file = 'data/firstStart.pkl'
+
+def loadfirstfile():
+    try:
+        with open(first_start_file, 'rb') as file:
+            return pickle.load(file)
+    except FileNotFoundError:
+        return None
 
 if os.path.exists(folder_path):
-    try:
-        with open(ifinfirst, 'rb') as file:
-            infirst = pickle.load(file)
-        if infirst == "yes":
-            print("Welcome to List Manager!")
-            print("View documentation in folder docs, or type help.")
-            print("")
-            os.remove(ifinfirst)
-    except FileNotFoundError:
-        print("")
-    print("Data load sucsesfull.")
+    if loadfirstfile() == 'yes':
+        print("Welcome to List Manager!")
+        print("See documentation in docs folder, or type help to see all commands.")
+        os.remove(first_start_file)
 else:
     print("Error. Not found necessary files. Please, check script plugins/plugin_main.py.")
     print("If you haven't this file, download it from GitHub. Here link: https://github.com/2012Miron/List-Manager")
@@ -41,17 +39,23 @@ while True:
         plugins.plugin_main.cmdCredits()
     elif CMDI == "list":
         plugins.plugin_main.cmdList()
-    elif CMDI[:4] == "elem":
+    elif CMDI.startswith("elem"):
         plugins.plugin_main.cmdElem(CMDI[5:])
-    elif CMDI[:4] == "edit":
+    elif CMDI.startswith("edit"):
         plugins.plugin_main.cmdEdit(CMDI[5:])
     elif CMDI == "mkel":
         plugins.plugin_main.cmdMakelement()
-    elif CMDI[:3] == "del":
+    elif CMDI[:4] == "del ":
         plugins.plugin_main.cmdDelete(CMDI[4:])
     elif CMDI == "save":
         plugins.plugin_main.saveData()
         print("data saved")
+    elif CMDI == "classes":
+        plugins.plugin_main.classes()
+    elif CMDI == "mkcls":
+        plugins.plugin_main.makeClass()
+    elif CMDI.startswith("delcls"):
+        plugins.plugin_main.deleteClass(CMDI[7:])
     elif CMDI == "cl":
         plugins.plugin_main.changeList()
     elif CMDI == "lists":
@@ -60,7 +64,7 @@ while True:
         plugins.plugin_main.currentList()
     elif CMDI == "mkli":
         plugins.plugin_main.makeList()
-    elif CMDI[:5] == "delli":
+    elif CMDI.startswith("delli"):
         plugins.plugin_main.deleteList(CMDI[6:])
     else:
         print("")
