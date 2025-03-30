69d7bf658b98c752b0db7b751ca043679132ba73
Miron <gsvitozar@gmail.com>
2025-03-30 21:19:07 +0300

    Added documentation for plugins, simplified import of plugins into General.py.

--- General.py
+++ General.py
@@ -1,6 +1,6 @@
 import os
 import pickle
-import plugins.plugin_main
+from plugins import plugin_main
 
 folder_path = 'data'
 first_start_file = 'data/firstStart.pkl'
@@ -29,43 +29,43 @@ while True:
     if CMDI == "exit":
         print("Goodbye! See you soon.")
         print("Closing program...")
-        plugins.plugin_main.saveData()
+        plugin_main.saveData()
         break
     elif CMDI == "help":
-        plugins.plugin_main.cmdHelp()
+        plugin_main.cmdHelp()
     elif CMDI == "version":
-        plugins.plugin_main.cmdVersion()
+        plugin_main.cmdVersion()
     elif CMDI == "credits":
-        plugins.plugin_main.cmdCredits()
+        plugin_main.cmdCredits()
     elif CMDI == "list":
-        plugins.plugin_main.cmdList()
+        plugin_main.cmdList()
     elif CMDI.startswith("elem"):
-        plugins.plugin_main.cmdElem(CMDI[5:])
+        plugin_main.cmdElem(CMDI[5:])
     elif CMDI.startswith("edit"):
-        plugins.plugin_main.cmdEdit(CMDI[5:])
+        plugin_main.cmdEdit(CMDI[5:])
     elif CMDI == "mkel":
-        plugins.plugin_main.cmdMakelement()
+        plugin_main.cmdMakelement()
     elif CMDI[:4] == "del ":
-        plugins.plugin_main.cmdDelete(CMDI[4:])
+        plugin_main.cmdDelete(CMDI[4:])
     elif CMDI == "save":
-        plugins.plugin_main.saveData()
+        plugin_main.saveData()
         print("data saved")
     elif CMDI == "classes":
-        plugins.plugin_main.classes()
+        plugin_main.classes()
     elif CMDI == "mkcls":
-        plugins.plugin_main.makeClass()
+        plugin_main.makeClass()
     elif CMDI.startswith("delcls"):
-        plugins.plugin_main.deleteClass(CMDI[7:])
+        plugin_main.deleteClass(CMDI[7:])
     elif CMDI == "cl":
-        plugins.plugin_main.changeList()
+        plugin_main.changeList()
     elif CMDI == "lists":
         print(os.listdir('data'))
     elif CMDI == "curlist":
-        plugins.plugin_main.currentList()
+        plugin_main.currentList()
     elif CMDI == "mkli":
-        plugins.plugin_main.makeList()
+        plugin_main.makeList()
     elif CMDI.startswith("delli"):
-        plugins.plugin_main.deleteList(CMDI[6:])
+        plugin_main.deleteList(CMDI[6:])
     else:
         print("")
         print("unknown command. type help to see all commands")
---
+++ docs/PLUGINS.md
@@ -0,0 +1,62 @@
+# Plugins
+### Why?
+With update 1.2, List Manager have plugin support. This file contaits documentation about plugins.
+## How to create plugin
+Before starting, remember _two things_:
+1. Plugin can have any name, but recommend to `plugin_` and your `name.py`.
+2. Plugin is need to **connect manually**, becouse LM don't have auto connect.
+
+Now, documentation:
+### 1. Create file.
+Download project, and in folder `plugins` create your file. Next, if this need, write code to list loading. Like this:
+```Python
+import os, ast
+
+if os.path.exists('data'):
+    with open(f'data/{List}', 'r') as file:
+        mainlist = file.read()
+    class_names = mainlist[mainlist.find('\n\n\n\n[') + 4:]
+    mainlist = mainlist.replace(mainlist[mainlist.find('\n\n\n\n'):], '')
+    mainlist = ast.literal_eval(mainlist)
+    class_names = ast.literal_eval(class_names)
+```
+
+Or, you can get mainlist from main plugin:
+```Python
+from plugin_main import mainlist, class_names
+```
+
+Now, you can use `mainlist` variable with data base.
+
+After, write functions to manipulate with list, or other functionality.
+Like this:
+```Python
+def createCopy():
+    are_want = input('Are you want to create copy of list? (yes, no):> ')
+    if are_want.lower() == 'yes':
+        with open('data/copy.lmdb', 'x') as file:
+            file.write(f'{mainlist}\n\n\n\n{class_names}')
+        print('Copy created succesful')
+        return
+    else:
+        print('Canceled')
+        return
+```
+
+### 2. Connect plugin to the program
+Save your plugin file, and go to `General.py`. Add import of your plugin, like this:
+```Python
+import os
+import pickle
+from plugins import plugin_main, plugin_my
+```
+
+And, between lines `plugin_main.deleteList(CMDI[6:])`, and `else:`, add commands of your plugin:
+```Python
+elif CMDI.startswith("delli"):
+    plugin_main.deleteList(CMDI[6:])
+elif CMDI == 'mkcopy':
+    plugin_my.createCopy()
+else:
+```
+Now you know, how create plugin for List Manager.
